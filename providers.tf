2e88e642fcbf48133e90587be3aff8c1debb53b1
kush bhargava <bhargavakush93@gmail.com>
2019-04-04 13:31:39 +0530

    added s3 backend

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "${var.s3_region}"
 }
+
+terraform {
+  backend "s3" {
+
+    bucket = "ftl-bucket-terraform-test-ap-south-1"
+    key = "test/terraform"
+    region = "ap-south-1"
+    encrypt = "true"
+  }
+}

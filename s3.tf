1bc04dbce64901a18a582129f8a11d38357f90fd
kush bhargava <bhargavakush93@gmail.com>
2019-04-04 11:45:46 +0530

    variable conf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,10 @@
 resource "aws_s3_bucket" "main" {
-  bucket = "terra-bucket-3"
+  bucket = "${var.s3_bucket_name}"
   acl    = "private"
+
+  tags = {
+    env = "${lookup(var.s3_tags, "environment")}"
+  }
+
+  region = "${var.s3_regions[0]}"
 }
